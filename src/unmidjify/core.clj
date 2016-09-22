b5ec31cafa4319b83cad74ac2459c38f587124ee
Amogh Talpallikar <amogh@helpshift.com>
2016-09-22 09:56:19 +0530

    Fixed doc strings

--- src/unmidjify/core.clj
+++ src/unmidjify/core.clj
@@ -1,83 +1,106 @@
 (ns unmidjify.core
-  (:gen-class)
   (:require [clojure.java.io :as io]
             [clojure.core.strint :refer [<<]]
             [clojure.tools.namespace :as ns]
             [clojure.core.match :refer [match]]
-            [clojure.string :as str]
+            [clojure.string :as cs]
             [clojure.pprint]
             [clojure.zip :as zip]
             [bultitude.core :as bultitude]))
 
 
-(defn third [lst]
+(defn third
+  "Get third element of a sequence or return nil."
+  [lst]
   (last (take 3 lst)))
 
-(defn regex? [token]
-  (= (type token) java.util.regex.Pattern))
+
+(defn regex?
+  "Check if the token is a regular expression or not"
+  [token]
+  (= (type token)
+     java.util.regex.Pattern))
 
 
-(defn re [token]
-  (re-pattern token))
+(defn  format-for-file
+  "Pretty print all the forms and return as text."
+  [forms]
+  (clojure.string/join
+   "\n"
+   (map (fn [form]
+          (with-out-str
+            (clojure.pprint/pprint form)))
+        forms)))
 
-(defn  format-for-file  [forms]
-  (reduce (fn [content form]
-            (str content "\n\n"
-                 (with-out-str
-                   (clojure.pprint/write
-                    form
-                    :dispatch clojure.pprint/code-dispatch))))
-          ""
-          forms))
-
-
-(defn list! [s]
-  {:post [(list? %)]}
-  (apply list s))
 
 (def arrow-symbols #{'=> '=not=> '=deny=> '=contains=>})
 
-(defn arrow? [[f1 f2 f3]]
+
+(defn arrow?
+  "Check if a form contains midje's arrow forms."
+  [[f1 f2 f3]]
   (contains? arrow-symbols f2))
 
-(defn get-arrow [[f1 f2 f3 :as f]]
+
+(defn get-arrow
+  "If there is arrow in the form, return it otherwise return nil"
+  [[f1 f2 f3 :as f]]
   (when (arrow? f)
     f2))
 
+
 (defn translate-regex-arrow
+  "Translate Midje's regex based arrow syntax to corresponding clojure.test syntax."
   [[f1 f2 f3]]
   (match [f1 f2 f3]
     [actual '=> re] `(~'is-re ~re ~actual)
-    [actual '=not=> re] `(~'is-not (~'re-find ~re ~actual))
-    [actual '=deny=> re] `(~'is-not (~'re-find ~re ~actual))))
+    [actual '=not=> re] `(~'is (~'not (~'re-find ~re ~actual)))
+    [actual '=deny=> re] `(~'is (~'not (~'re-find ~re ~actual)))))
 
-(defn translate-contains-arrow [[f1 f2 f3]]
+
+(defn translate-contains-arrow
+  "Translate Midje's 'contains' arrow syntaxt to a suitable
+  clojure.test replacement."
+  [[f1 f2 f3]]
   {:post [(do (assert % (<< "failed: ~{f1} ~{f2} ~{f3}")) true)]}
   (let [[_ contents & options] f3]
     (match [f1 f2 contents]
       [actual '=> (_ :guard string?)] `(~'is (~'=> ~f3 ~actual))
       [actual '=> (_ :guard map?)] `(~'is (~'submap? ~contents ~actual))
       [actual '=> (_ :guard regex?)] `(~'is (~'re-find ~contents ~actual))
-      [actual '=not=> (_ :guard string?)] `(~'is-not (~'re-find ~(re contents) ~actual))
-      [actual '=not=> (_ :guard regex?)] `(~'is-not (~'re-find ~contents ~actual))
+      [actual '=not=> (_ :guard string?)] `(~'is (~'not (~'re-find ~(re-pattern contents) ~actual)))
+      [actual '=not=> (_ :guard regex?)] `(~'is (~'not (~'re-find ~contents ~actual)))
       [actual arrow contents] `(~'is (~arrow ~f3 ~actual)))))
+
+
+(defn form-is-anon-fn?
+ [item]
+ "Check if the item is an anonymous function"
+ (and (list? item)
+      (= (first item) 'fn)))
+
 
 (defn form-is-fn?
   "Resolves a form, and returns truthy if it evaluates to a fn"
   [f]
-  (when (symbol? f)
+  (fn? f)
+  (if (symbol? f)
     (let [resolved (resolve f)]
       (and resolved (or (fn? resolved)
                         (and (var? resolved)
-                             (fn? @resolved)))))))
+                             (fn? @resolved)))))
+    (form-is-anon-fn? f)))
 
-(defn translate-normal-arrow [[f1 f2 f3]]
+
+(defn translate-normal-arrow
+  "Translate regular midje arrow syntax to clojure.test form."
+  [[f1 f2 f3]]
   (match [f1 f2 f3]
     [actual '=> true] `(~'is ~actual)
     [actual '=> 'truthy] `(~'is ~actual)
     [actual '=> 'anything] `(~'is (~'anything ~actual))
-    [actual '=> false] `(~'is-not ~actual)
-    [actual '=> 'falsey] `(~'is-not ~actual)
+    [actual '=> false] `(~'is (~'not ~actual))
+    [actual '=> 'falsey] `(~'is (~'not ~actual))
     [actual '=> (f :guard form-is-fn?)] `(~'is (~f ~actual))
     [actual '=> 0] `(~'is (~'zero? ~actual))
     [actual '=> nil] `(~'is (~'nil? ~actual))
@@ -85,13 +108,18 @@
     [actual '=contains=> (expected :guard map?)] `(~'is (~'submap? ~expected ~actual))
     [actual '=not=> expected] `(~'is (~'not= ~expected ~actual))))
 
-(defn translate-throws [[f1 f2 f3]]
+
+(defn translate-throws
+  "Translate Midje's throws to something else."
+  [[f1 f2 f3]]
   (let [exception-form (rest f3)]
+    (println exception-form)
     (condp = (count exception-form)
       0 `(~'is (~'thrown? Exception ~f1))
       1 `(~'is (~'thrown? ~@exception-form ~f1))
       2 `(~'is (~'thrown-with-msg? ~@exception-form ~f1))
       :else (assert false))))
+
 
 (defn translate-arrow [[f1 f2 f3 :as f]]
   {:post [(do (assert % (<< "failed: ~{f1} ~{f2} ~{f3}")) true)]}
@@ -104,6 +132,7 @@
      contains-form? (translate-contains-arrow f)
      thrown? (translate-throws f)
      :else (translate-normal-arrow f))))
+
 
 (defn replace-arrow
   "If the head of the form is (foo) => (bar), return clojure.test
@@ -118,23 +147,28 @@
               expected (third lst)]
           (recur (conj ret (translate-arrow lst)) (drop 3 lst)))
         (recur (conj ret (first lst)) (rest lst)))
-      (list! ret))))
+      ret)))
 
-(defn munge-name [name]
+
+(defn munge-name
+  "Turn a string into a valid clojure identifier name"
+  [name]
   (-> name
-      (str/replace " " "-")
-      (str/replace "." "")
-      (str/replace "`" "")
-      (str/replace "'" "")
-      (str/replace "(" "")
-      (str/replace ")" "")
-      (str/replace "/" "")
-      (str/replace "," "-")
-      (str/replace "[" "")
-      (str/replace "]" "")
-      (str/replace #"^(\d+)" "_$1")
-      (str/replace #"^:" "")
-      (str/replace #"-+" "-")))
+      (cs/replace " " "-")
+      (cs/replace "\n" "")
+      (cs/replace "." "")
+      (cs/replace "`" "")
+      (cs/replace "'" "")
+      (cs/replace "(" "")
+      (cs/replace ")" "")
+      (cs/replace "/" "")
+      (cs/replace "," "-")
+      (cs/replace "[" "")
+      (cs/replace "]" "")
+      (cs/replace #"^(\d+)" "_$1")
+      (cs/replace #"^:" "")
+      (cs/replace #"-+" "-")
+      (.toLowerCase)))
 
 
 (defn replace-fact
@@ -142,32 +176,35 @@
   [form]
   (if (= 'fact (first form))
     (let [[fact name & body] form]
-      (list! `(~'deftest ~(symbol (munge-name name)) ~@body)))
+      `(~'deftest ~(symbol (munge-name name)) ~@body))
     form))
 
-(defn replace-midje-sweet [form]
+
+(defn replace-midje-sweet
+  "Replace midje imports with clojure.test"
+  [form]
   (if (= 'midje.sweet form)
     'clojure.test
     form))
 
-(defn replace-future-fact [form]
+
+(defn replace-future-fact
+  "Replace future facts with commented deftest"
+  [form]
   (if (= 'future-fact (first form))
     `(~'comment ~(replace-fact `(~'fact ~@(rest form))))
     form))
 
-(defn cast-coll
-  "Convert one seq class into another. Use it to handle (into (empty list) '(1 2 3)) being dumb"
-  [cls form]
-  (condp = cls
-    clojure.lang.PersistentList (list* form)
-    clojure.lang.PersistentVector (vec form)))
 
 (defn test-relative-path
+  "Relative path from the test directory"
   [absolute-path]
   (first
    (re-find #"/test(/.*)?$" absolute-path )))
 
+
 (defn file-path-excluded?
+  "Check of file-path is in the list of excluded-paths."
   [excluded-paths file-path]
   (let [relative-file-path (test-relative-path file-path)]
     (not
@@ -175,90 +212,117 @@
       (filter
        (fn [excluded-path]
          (let [relative-excluded-path (test-relative-path excluded-path)]
-           (re-find (re-pattern (str "^" relative-excluded-path)) relative-file-path)))
+           (re-find (re-pattern (str "^" relative-excluded-path))
+                    relative-file-path)))
        excluded-paths)))))
 
-(defn wrap-fact-body-in-let [let-bindings fact-form]
-  (let [fact-content (drop 2 fact-form)]
+
+(defn wrap-deftest-body-in-let
+  "Surround a given fact form with let bindings."
+  [let-bindings deftest-form]
+  (let [deftest-content (drop 2 deftest-form)]
     (list
-     'fact
-     (nth fact-form 1)
+     'deftest
+     (nth deftest-form 1)
      (concat
       (list 'let let-bindings)
-      fact-content))))
+      deftest-content))))
 
-(defn push-outer-let-in-each-fact 
+
+(defn push-outer-let-in-each-deftest
+  "Bring an outer let form and push it inside each fact form within."
   [form]
   (let [let-bindings (nth form 1)
         let-body (filter
-                  #(= (first %) 'fact)
+                  #(= (first %) 'deftest)
                   (drop 2 form))
         let-wrapped-forms (map
-                           #(wrap-fact-body-in-let let-bindings %)
+                           #(wrap-deftest-body-in-let let-bindings %)
                            let-body)]
-
     let-wrapped-forms))
 
-(defn fact-in-let?
+
+(defn deftest-in-let?
+  "Checks if the form is a let form that contains a face form or not."
   [form]
   (and (= (first form) 'let)
-       (some #(and (list? %)
-                   (= (first %) 'fact))
+       (some #(and (seq? %)
+                   (= (first %) 'deftest))
              (drop 2 form))))
 
 
-(defn fact-forms-wrapped-let
+(defn vectorify-if
+  "if is-vector is true, then convert seq to vector."
+  [is-vector seq]
+  (if is-vector
+    (vec seq)
+    seq))
+
+
+(defn walk-form
+  "Walk through the form recursively
+  and replace inline midje code with clojure.test."
   [form opts]
-  (if-not (file-path-excluded? (get opts :exclude-paths #{}) (opts :file-path))
-    (push-outer-let-in-each-fact form)
-    '(form)))
-
-
-(defn munge-form [form opts]
-  ;;(update-nested-facts form opts)
   (let [form (-> form
                  (replace-midje-sweet))
-        form-class (class form)]
+        is-vector (vector? form)]
     (cond
-     (or (list? form)
-         (vector? form)) (-> form
-                             (replace-fact)
-                             (replace-future-fact)
-                             (replace-arrow)
-                             (->> (map #(munge-form % opts))
-                                  (cast-coll form-class)))
-     :else form)))
+      (or (seq? form)
+          (vector? form)) (-> form
+                              (replace-fact)
+                              (replace-future-fact)
+                              (replace-arrow)
+                              (->> (map #(walk-form % opts))
+                                   (vectorify-if is-vector)))
+      :else form)))
 
 
-(defn walk [form opts]
-  (munge-form form opts))
+(defn walk
+  "Walk through though the form."
+  [form opts]
+  (walk-form form opts))
 
-(defn read-seq [s]
+
+(defn read-seq
+  "Read clojure source code as a list of form"
+  [s]
   (read-string (str "(" s  "\n)")))
 
+
 (defn ns-state-change-form?
+  "Check if the form is namespace-state-change form."
   [form]
   (= 'namespace-state-changes
      (first form)))
 
-(defn get-ns-forms [forms]
+
+(defn get-ns-forms
+  "Filter out all namepace state changes forms"
+  [forms]
   (filter
    ns-state-change-form?
    forms))
 
-(defn without-ns-forms [forms]
+
+(defn without-ns-forms
+  "Remove all namespace state changes forms"
+  [forms]
   (remove
    ns-state-change-form?
    forms))
 
+
 (defn extract-ns-vectors
+  "Extract vectors that are part of namespace-state-changes form"
   [ns-forms]
   (map
    (fn [ns-form]
     (last ns-form))
    ns-forms))
 
+
 (defn get-fixture-map
+  "Get a fixture map."
   [ns-forms]
   (reduce
    (fn [fixture-map curr-form]
@@ -266,29 +330,40 @@
    {}
    ns-forms))
 
+
 (defn define-once-fixture
   [fixture-map]
+  "Define fixture that runs once."
   (let [fixture-code '(defn once-fixture [tests]
                         :before
                         (tests)
                         :after)]
-    (replace {:before (fixture-map 'before) 
-              :after (fixture-map 'after)} fixture-code)))
+    (replace {:before (fixture-map 'before)
+              :after (fixture-map 'after)}
+             fixture-code)))
+
 
 (defn define-each-fixture
+  "Define fixture that runs for each test."
   [fixture-map]
   (let [fixture-code '(defn each-fixture [tests])]
     (concat
      fixture-code
-     [(clojure.walk/postwalk-replace {'?form '(tests)} (fixture-map 'around))])))
+     [(clojure.walk/postwalk-replace
+       {'?form '(tests)}
+       (fixture-map 'around))])))
+
 
 (defn add-fixture-code
+  "Add fixture code to forms."
   [forms & fixture-code]
   (concat (list (first forms))
           fixture-code
           (rest forms)))
 
+
 (defn replace-ns-state-changes
+  "Replace namespace state changes with corresponding clojure.test fixtures"
   [forms]
   (let [fixture-map (->> forms
                          (get-ns-forms)
@@ -307,24 +382,32 @@
               '(use-fixtures :each  each-fixture))
              %)))))
 
+
 (defn facts?
+  "Check if the form a facts form"
   [element]
   (and
    (seq? element)
    (= (first element) 'facts)))
 
+
 (defn contains-facts-form?
+  "Check if the form-list contains a facts form."
   [form-list]
   (first
    (filter
     facts?
     form-list)))
 
+
 (defn facts-content
+  "Get content within facts form"
   [facts-form]
   (drop 2 facts-form))
 
+
 (defn replace-facts-with-content
+  "Replace facts with it's content."
   [item]
   (if (seq? item)
     (mapcat
@@ -335,24 +418,30 @@
      item)
    item))
 
+
 (defn open-all-facts
+  "Open up all facts forms and bring the inner content out."
   [form-list]
   (clojure.walk/postwalk
    replace-facts-with-content 
    form-list))
 
-(defn unbound-let-bound-fact
+
+(defn unbound-let-bound-test
+  "Remove the let wrapping up the fact form and bring it inside of it."
   [form-list opts]
-  (let [fact-wrapped-let (->> form-list
-                             (filter fact-in-let?)
-                             (map #(fact-forms-wrapped-let % opts))
-                             (apply concat))]
+  (let [deftest-wrapped-let (->> form-list
+                                 (filter deftest-in-let?)
+                                 (mapcat #(push-outer-let-in-each-deftest %)))]
     (->>
      form-list
-     (remove fact-in-let?)
-     (#(concat %  fact-wrapped-let)))))
+     (remove deftest-in-let?)
+     (#(concat %  deftest-wrapped-let)))))
 
-(defn extract-action [action-symbol actions]
+
+(defn extract-action
+  "Extracts the specified code."
+  [action-symbol actions]
   (last
    (first
     (filter
@@ -360,7 +449,10 @@
            (= (nth % 1) :checks))
      actions))))
 
+
 (defn with-state-changes-inside-out
+  "Read 'before' and 'after' actions from midje's with-state-change form and
+   wrap all fact forms with the code around them."
   [[form-name actions facts-form :as form]]
   (if (= form-name 'with-state-changes)
     (let [before-action (extract-action 'before actions)
@@ -373,43 +465,122 @@
       facts-form))
     form))
 
-(defn transform [file opts]
+
+(defn open-with-state-changes
+  "Open up with-state-changes changes form"
+  [form-list]
+  (map
+   with-state-changes-inside-out
+   form-list))
+
+
+(defn transform-comment
+  "Converts a single line comment into a string with tag like markup to
+   preserve comments in a single line after reading and pretty printing."
+  [source-code]
+  (clojure.string/replace
+   source-code #"\n\s*(;.+)"
+   (fn [[fst & ignore]]
+     (str "\"<COMMENT>"
+          (cs/replace fst #"\"" {"\"" "__qt__"})
+          "</COMMENT>\""))))
+
+
+(defn recover-comment
+  "Recover comment back from a format
+  of \"<COMMENT>;; This is comment</COMMENT>\" back to it's original form. "
+  [source-code]
+  (clojure.string/replace
+   source-code #"\"<COMMENT>\s*(.*)\s*</COMMENT>\""
+   (fn [[fst sec]]
+     (->> sec
+          (#(cs/replace % #"__qt__" {"__qt__" "\""}))
+          (re-find #";(.+)")
+          (first)))))
+
+
+(defn prefix-name-space
+  "Insert 'cljtest' in name space and second last position."
+  [form-list]
+  (map
+   (fn [form]
+     (if (= (first form) 'ns)
+       (let [[_  ns-name] form
+             new-ns (-> ns-name
+                        (name)
+                        (cs/split #"\.")
+                        (#(concat (butlast %)
+                                  (list "cljtest")
+                                  (list (last %))))
+                        (#(cs/join "." %))
+                        (symbol))]
+         (replace {ns-name new-ns} form))
+       form)) form-list))
+
+
+(defn transform
+  "Takes a file through series of transformations."
+  [file opts]
   (-> file
-      slurp
+      (slurp)
+      (#(if (:keep-comments opts) (transform-comment %) %))
       (read-seq)
-      (unbound-let-bound-fact opts)
-      (list!)
+      (open-with-state-changes)
       (walk opts)
       (replace-ns-state-changes)
-      (#(map with-state-changes-inside-out %))
       (open-all-facts)
-      (format-for-file)))
+      (unbound-let-bound-test opts)
+      (prefix-name-space)
+      (format-for-file)
+      (#(if (:keep-comments opts) (recover-comment %) %))))
 
-(defn midje? [file]
+
+(defn midje?
+  "Check if the file contains midje related code or not."
+  [file]
   (or (re-find #"midje.sweet" (slurp file))
       (re-find #"\(fact" (slurp file))))
 
-(defn original-file? [file]
-  (re-find #"cljtest" (.getAbsolutePath file)))
 
-(defn- backup-copy-name [f]
-  (str 
+(defn original-file?
+  "Checks if the file is original source code or not."
+  [file]
+  (not
+   (re-find #"cljtest" (.getAbsolutePath file))))
+
+
+(defn- new-source-name
+  "Returns a full-qualified name for the newly generated source file"
+  [f]
+  (str
    (.getParent f)
    "/cljtest/"
    (.getName f)))
 
-(defn- new-source-file [f content]
-  (clojure.java.io/make-parents (backup-copy-name f))
-  (spit (backup-copy-name f) content))
 
-(defn- unmidjify-file [f opts]
+(defn- new-source-file
+  "Create a backup file for the source file.
+   Spits the content out to a new file 
+  and creates every directory and file that is not part of the path"
+  [f content]
+  (clojure.java.io/make-parents (new-source-name f))
+  (spit (new-source-name f) content))
+
+
+(defn- unmidjify-file
+  "Convert a midje source file to clojure.test source file."
+  [f opts]
   (-> f
       (transform (assoc opts :file-path (.getAbsolutePath f)))
       (->> (new-source-file f))))
 
-(defn- unmidjify-dir [d opts]
+
+(defn- unmidjify-dir
+  "Recursively walk down a directory
+  and unmidjify each file that contains midje forms."
+  [d opts]
   (let [files (->> (java.io.File. d)
-                   (ns/find-clojure-sources-in-dir )
+                   (ns/find-clojure-sources-in-dir)
                    (filter midje?)
                    (filter original-file?))]
     (doseq [f files]
@@ -419,6 +590,7 @@
           (print t)
           (.printStackTrace t)
           (println "FAILED:" f))))))
+
 
 (defn unmidjify
   "Checks if the path is a single file.
@@ -431,10 +603,4 @@
     (if (.isDirectory (io/file path))
       (unmidjify-dir path (or opts {}))
       (unmidjify-file (java.io.File. path) (or opts {})))))
-
-
-
-
-
-
 
